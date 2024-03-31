5596a5ae3fe5eb1aacbc0b75d93433d4aba7108b
Grace <gxu660@aucklanduni.ac.nz>
2024-03-31 18:18:15 +1300

    Alter the way empty inputs are addressed, and allow for button-click via ENTER key, to prioritise user comfort

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const inputBox = document.getElementById("input-box");
 const listWrapper = document.getElementById("list-wrapper");
 function addItem() {
-  if(inputBox.value === "") {
-    alert("The task can't be empty!");
-  } else {
+  if(!(inputBox.value === "")) {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listWrapper.appendChild(li);
@@ -36,3 +34,11 @@ function showState() {
 }
 
 showState();
+
+var input = document.getElementById("input-box")
+input.addEventListener("keypress", function(v) {
+  if (v.key === "Enter") {
+    v.preventDefault();
+    document.getElementById("add-button").click();
+  }
+});
